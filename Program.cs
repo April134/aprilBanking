3f826a6f70e6bc7d94c1f2d1a4eaf76a928cb2f0
April134 <144878429+April134@users.noreply.github.com>
2023-09-13 13:53:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    static void DisplayAccountDetails(Account account8){
+        Console.WriteLine($"Account Number: {account8.AccountNumber}");
+        Console.WriteLine($"Balance: ${account8.Balance}");
+    }
+    static void Main(string [] args){
+        SavingsAccount savingsAccount = new SavingsAccount(1500, 34.0);
+        CheckingAccount checkingAccount = new CheckingAccount(1999,24.0);
+
+        savingsAccount.Deposit(5000);
+        DisplayAccountDetails(savingsAccount);
+        savingsAccount.CalculateInterest();
+        DisplayAccountDetails(savingsAccount);
+
+        Console.WriteLine();
+
+        checkingAccount.Deposit(100);
+        DisplayAccountDetails(checkingAccount);
+        checkingAccount.Withdraw(600);
+        DisplayAccountDetails(checkingAccount);
+    }
+}
